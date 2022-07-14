f65104aa582274f1efb57663c5a9c6f0a7787ca0
sianturiwilly <prajudiwilliam10@gmail.com>
2022-07-14 15:25:58 +0700

    Third commit

---
+++ tutorial/src/video5/DataDiri.java
@@ -0,0 +1,30 @@
+package video5;
+
+public class DataDiri {
+
+    public static void main(String[] args){
+        
+       // Membuat Variabel
+       /*
+            - Format Penulisan Variabel
+                <tipe data> namaVariabel
+       */
+
+       String nama, alamat;
+       int usia;
+       Double tinggi;
+
+       // Mengisi variabel
+       nama = "Willi Chrisdeardo Sianturi";
+       alamat = "Bekasi, Jawa Barat";
+       usia = 29;
+       tinggi = 177.0;
+
+       // Mencetak ke layar isi variabel
+       System.out.println("Nama = " + nama);
+       System.out.println("Alamat = " + alamat);
+       System.out.println("Usia = " + usia + " tahun");
+       System.out.println("Tinggi = " + tinggi);
+
+    }
+}
