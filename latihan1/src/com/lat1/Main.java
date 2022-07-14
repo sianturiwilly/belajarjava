3b5717c868365096cff4e5ec94499bc6f171ee00
sianturiwilly <prajudiwilliam10@gmail.com>
2022-07-14 11:23:57 +0700

    first commit

---
+++ latihan1/src/com/lat1/Main.java
@@ -0,0 +1,10 @@
+package com.lat1;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Salam Ngodingpintar");
+
+        System.out.println("Halo dunia, selamat siang teman-teman.");
+    }
+
+}
